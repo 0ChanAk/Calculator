62166d4077c7e7cd69d48c54dfb1dc082ae182e4
Chandana Akriti <chandanaakriti9@gmail.com>
2018-12-30 13:25:54 +0530

    updated

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,109 +1,162 @@
+# Python program to  create a simple GUI  
+# calculator using Tkinter 
+  
+# import everything from tkinter module 
 from tkinter import *
-
-def btnClick(numbers):
-    global operator
-    operator = operator + str(numbers)
-    text_Input.set(operator)
-
-def btnClear():
-    global operator
-    operator = ""
-    text_Input.set("")
-    
-def btnEquals():
-    try:
-        
-        global operator
-        sumup = str(eval(operator))
-        text_Input.set(sumup)
-        operator = ""
-
-    except:
-
-        text_Input.set(" ERROR ")
-        operator
-        
-
-
-cal = Tk()
-
-cal.title("Calculator")
-operator=""
-text_Input = StringVar()
-
-txtDisplay = Entry(cal, font=('arial', 20, 'bold'), textvariable = text_Input, bd=30,
-                   insertwidth = 3, bg = "cyan", justify = 'right').grid(columnspan=4)
-
-
-button7 = Button(cal, padx=16,pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="7",
-                 command = lambda:btnClick(7))
-button7.grid(row=1,column=0)
-
-button8 = Button(cal, padx=16,pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="8",
-                 command = lambda:btnClick(8))
-button8.grid(row=1,column=1)
-
-button9 = Button(cal, padx=16,pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="9",
-                 command = lambda:btnClick(9))
-button9.grid(row=1,column=2)
-
-buttonMul = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="*",
-                  command = lambda:btnClick("*"))
-buttonMul.grid(row=1,column=3)
-
-############################################################################################
-
-button4 = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="4",
-                 command = lambda:btnClick(4))
-button4.grid(row=2,column=0)
-
-button5 = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="5",
-                 command = lambda:btnClick(5))
-button5.grid(row=2,column=1)
-
-button6 = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="6",
-                 command = lambda:btnClick(6))
-button6.grid(row=2,column=2)
-
-buttonAdd = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="+",
-                  command = lambda:btnClick("+"))
-buttonAdd.grid(row=2,column=3)
-                  
-##############################################################################################
-
-button1 = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="1",
-                 command = lambda:btnClick(1))
-button1.grid(row=3,column=0)
-
-button2 = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="2",
-                 command = lambda:btnClick(2))
-button2.grid(row=3,column=1)
-
-button3 = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="3",
-                 command = lambda:btnClick(3))
-button3.grid(row=3,column=2)
-
-buttonSub = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="-",
-                  command = lambda:btnClick("-"))
-buttonSub.grid(row=3,column=3)
-
-###############################################################################################
-
-button0 = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="0",
-                  command = lambda:btnClick("0"))
-button0.grid(row=4,column=0)
-
-buttonClr = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="c",
-                  command = lambda:btnClear)
-buttonClr.grid(row=4,column=1)
-
-buttonEqu = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="=",
-                  command = lambda:btnEquals)
-buttonEqu.grid(row=4,column=2)
-
-buttonDiv = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="/",
-                  command = lambda:btnClick("/"))
-buttonDiv.grid(row=4,column=3)
-
-
-cal.mainloop()
+  
+# globally declare the expression variable 
+expression = "" 
+  
+  
+# Function to update expressiom 
+# in the text entry box 
+def press(num): 
+    # point out the global expression variable 
+    global expression 
+  
+    # concatenation of string 
+    expression = expression + str(num) 
+  
+    # update the expression by using set method 
+    equation.set(expression) 
+  
+  
+# Function to evaluate the final expression 
+def equalpress(): 
+    # Try and except statement is used 
+    # for handling the errors like zero 
+    # division error etc. 
+  
+    # Put that code inside the try block 
+    # which may generate the error 
+    try: 
+  
+        global expression 
+  
+        # eval function evaluate the expression 
+        # and str function convert the result 
+        # into string 
+        total = str(eval(expression)) 
+  
+        equation.set(total) 
+  
+        # initialze the expression variable 
+        # by empty string 
+        expression = "" 
+  
+    # if error is generate then handle 
+    # by the except block 
+    except: 
+  
+        equation.set(" ERROR ") 
+        expression = "" 
+  
+  
+# Function to clear the contents 
+# of text entry box 
+def clear(): 
+    global expression 
+    expression = "" 
+    equation.set("") 
+  
+  
+# Driver code 
+if __name__ == "__main__": 
+    # create a GUI window 
+    gui = Tk() 
+  
+    # set the background colour of GUI window 
+    gui.configure(background="#D7BFDC") 
+  
+    # set the title of GUI window 
+    gui.title("Simple Calculator") 
+  
+    # set the configuration of GUI window 
+    gui.geometry("515x330") 
+  
+    # StringVar() is the variable class 
+    # we create an instance of this class 
+    equation = StringVar() 
+  
+    # create the text entry box for 
+    # showing the expression . 
+    expression_field = Entry(gui, font=('arial', 20, 'bold'), textvariable=equation, bd=30,
+                              insertwidth = 3, bg = "#FCF4A4",justify = 'right')
+  
+    # grid method is used for placing 
+    # the widgets at respective positions 
+    # in table like structure . 
+    expression_field.grid(columnspan=4, ipadx=70, ipady=10) 
+  
+    equation.set('Enter Your Expression') 
+  
+    # create a Buttons and place at a particular 
+    # location inside the root window . 
+    # when user press the button, the command or 
+    # function affiliated to that button is executed . 
+    button1 = Button(gui, text=' 1 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(1), height=1, width=7) 
+    button1.grid(row=2, column=0) 
+  
+    button2 = Button(gui, text=' 2 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(2), height=1, width=7) 
+    button2.grid(row=2, column=1) 
+  
+    button3 = Button(gui, text=' 3 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(3), height=1, width=7) 
+    button3.grid(row=2, column=2) 
+  
+    button4 = Button(gui, text=' 4 ', fg='black', font=('arial', 20, 'bold'),bg='#FFE5B4', 
+                     command=lambda: press(4), height=1, width=7) 
+    button4.grid(row=3, column=0) 
+  
+    button5 = Button(gui, text=' 5 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(5), height=1, width=7) 
+    button5.grid(row=3, column=1) 
+  
+    button6 = Button(gui, text=' 6 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(6), height=1, width=7) 
+    button6.grid(row=3, column=2) 
+  
+    button7 = Button(gui, text=' 7 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(7), height=1, width=7) 
+    button7.grid(row=4, column=0) 
+  
+    button8 = Button(gui, text=' 8 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(8), height=1, width=7) 
+    button8.grid(row=4, column=1) 
+  
+    button9 = Button(gui, text=' 9 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(9), height=1, width=7) 
+    button9.grid(row=4, column=2) 
+  
+    button0 = Button(gui, text=' 0 ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                     command=lambda: press(0), height=1, width=7) 
+    button0.grid(row=5, column=0) 
+  
+    plus = Button(gui, text=' + ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                  command=lambda: press("+"), height=1, width=7) 
+    plus.grid(row=2, column=3) 
+  
+    minus = Button(gui, text=' - ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                   command=lambda: press("-"), height=1, width=7) 
+    minus.grid(row=3, column=3) 
+  
+    multiply = Button(gui, text=' * ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                      command=lambda: press("*"), height=1, width=7) 
+    multiply.grid(row=4, column=3) 
+  
+    divide = Button(gui, text=' / ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                    command=lambda: press("/"), height=1, width=7) 
+    divide.grid(row=5, column=3) 
+  
+    equal = Button(gui, text=' = ', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                   command=equalpress, height=1, width=7) 
+    equal.grid(row=5, column=2) 
+  
+    clear = Button(gui, text='Clear', fg='black',font=('arial', 20, 'bold'), bg='#FFE5B4', 
+                   command=clear, height=1, width=7) 
+    clear.grid(row=5, column='1') 
+  
+    # start the GUI 
+    gui.mainloop() 
